42c94b696ba1618a4b0a92ea7c23b3c400efe740
Paul Robinson <paulalex675@hotmail.com>
2020-12-07 22:36:15 +0000

    the rules and feedback are now correct

--- simple_code_cracker.rb
+++ simple_code_cracker.rb
@@ -9,18 +9,14 @@ class Player
     end
 
     def add_guesses
-        puts "How many guesses do you think you can crack it in? (The standard amount is 12)"
-        puts "You'll get a feedback array after each guess. 
-          An 'I' will indicate you guessed one color and location correctly.
-          An '0' will indicate a correct color but incorrect location. 
-          An 'X' will indicate and incorrect color and location"
+        puts "How many guesses do you think you can crack it in?"
+        puts "I'll give you some feed back on how good each guess was"
         @num_guesses = gets.chomp.to_i
     end
 
     def turn
         while @num_guesses > 0
-            puts "Please take a guess, type 'r' for red, 'o' for orange, 'y' for yellow, 'g' for green, 
-            'b' for blue and 'p' for purple"
+            puts "Please take a guess, r for red, g for green, b for blue"
             puts "You have #{@num_guesses} guesses left"
             @guess.clear
             @guess << gets.chomp.split("") 
@@ -28,7 +24,6 @@ class Player
             @num_guesses -= 1
             feedback()
             if @guess == $code || @f_back == $code
-                print $code
                 puts "Congratulations you cracked my code!"
                 puts "You clever clogs"
                 exit
@@ -37,22 +32,22 @@ class Player
                     puts "Guess again"
                     turn()
                 else puts "You failed to crack my code, tough luck!"
-                    print $code
                 end
             end
         end
     end
 
     def feedback
+        a = 0
         @f_back.clear
-        @guess.each_index do |a| if @guess[a] === $code[a]
-                                @f_back << "I"
-                            elsif @guess.each { |b| $code.include? b }
-                                @f_back << "X"
-                            else @f_back << "0"
-                            end
-                    end
-    print @f_back.shuffle
+        while a < $code.length
+            if $code[a] == @guess[a]
+                @f_back << $code[a]
+            else @f_back << "*"
+            end
+            a += 1
+        end
+        print  @f_back
     end
 
 end
@@ -60,22 +55,24 @@ end
 class Code
     attr_accessor :code
     def initialize
-      @colors = ["r", "o", "y", "g", "b", "p"]
+      @colors = ["r", "g", "b"]
       $code = code
       code_generator()
     end
 
     def code_generator()
       $code = Array.new
-      4.times { $code << @colors.sample }
-      puts "Your 4 digit code contains a possible combination of the colors red, green, blue, yellow, purple and orange"
+      x = gets.chomp.to_i
+      x.times { $code << @colors.sample }
+      puts "Your code contains a combination of the colors red, green and blue"
     end
 end
 
 puts "Hello, I'm Mastermind!"
 puts "What is your name?"
 player1 = Player.new(gets.chomp)
-puts "Hi #{player1.name}, I bet you can't crack my colorful code, I've made it just for you!"
+puts "I bet you can't crack my colorful code"
+puts "#{player1.name} the code will be 4 digits long and will be a combination of 6 possible colours"
 Code.new
 player1.add_guesses
 player1.turn
